263159c46efeaf42219aea879da270fbd89d14ab
Pavel Loginov <lgnv2009@gmail.com>
2021-01-24 12:14:30 +0300

    Implement CMake build script

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+#include <GLFW/glfw3.h>
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    glm::vec3 v = glm::vec3(1.0f, 2.0f, 3.0f);
+    std::cout << "y = " << v.y << std::endl;
     return 0;
 }
