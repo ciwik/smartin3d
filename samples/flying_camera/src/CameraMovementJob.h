c422ed98b48052acabf742f88cc9a1aeab7660dd
Pavel Loginov <lgnv2009@gmail.com>
2021-03-02 20:08:16 +0300

    Moved demo project to 'samples' directory

--- samples/flying_camera/src/CameraMovementJob.h
+++ samples/flying_camera/src/CameraMovementJob.h
@@ -1,3 +1,6 @@
+#ifndef SMARTIN3D_CAMERAMOVEMENTJOB_H
+#define SMARTIN3D_CAMERAMOVEMENTJOB_H
+
 #include "base/App.h"
 
 using namespace smartin;
@@ -57,49 +60,5 @@ private:
     float speed, turnSpeed;
 };
 
-class XwingDestroyJob : public base::Job {
-public:
-    void Tick() override {
-        if (utils::input::keyboard::IsKeyDown(KEY_SPACE))
-            utils::DestroyActor(utils::FindActor("xwing"));
 
-        if (utils::input::keyboard::IsKeyDown(KEY_LEFT_SHIFT))
-            utils::DestroySkybox();
-
-        if (utils::input::keyboard::IsKeyDown(KEY_LEFT_CONTROL))
-            utils::DebugPrintContent();
-    }
-};
-
-int main() {
-    auto app = new base::App("Test", 1280, 720);
-    app->Init();
-
-    app->CreateCamera(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, -60.0f, 0.0f));
-    app->SetSkybox({
-       "cupertin-lake_rt.tga",
-       "cupertin-lake_lf.tga",
-       "cupertin-lake_up.tga",
-       "cupertin-lake_dn.tga",
-       "cupertin-lake_bk.tga",
-       "cupertin-lake_ft.tga"
-    });
-
-    app->AddActor("xwing", "x-wing.obj",
-                  glm::vec3(-7.0f, 0.0f, 10.0f),
-                  glm::vec3(0.006f, 0.006f, 0.006f));
-    app->AddActor("blackhawk", "uh60.obj",
-                  glm::vec3(-8.0f, 2.0f, 0.0f),
-                  glm::vec3(0.4f, 0.4f, 0.4f),
-                  glm::vec3(-90.0f, 0.0f, 0.0f));
-    app->AddActor("cone", "cone.obj",
-                  glm::vec3(-8.0f, 2.0f, 5.0f));
-
-    app->AddJob(std::make_unique<CameraMovementJob>(app->GetCamera(), 2.0f, 0.5f));
-    app->AddJob(std::make_unique<XwingDestroyJob>());
-
-    app->Run();
-    delete app;
-
-    return 0;
-}
+#endif //SMARTIN3D_CAMERAMOVEMENTJOB_H
---
+++ samples/flying_camera/src/XwingDestroyJob.h
@@ -0,0 +1,22 @@
+#ifndef SMARTIN3D_XWINGDESTROYJOB_H
+#define SMARTIN3D_XWINGDESTROYJOB_H
+
+#include "base/App.h"
+
+using namespace smartin;
+
+class XwingDestroyJob : public base::Job {
+public:
+    void Tick() override {
+        if (utils::input::keyboard::IsKeyDown(KEY_SPACE))
+            utils::DestroyActor(utils::FindActor("xwing"));
+
+        if (utils::input::keyboard::IsKeyDown(KEY_LEFT_SHIFT))
+            utils::DestroySkybox();
+
+        if (utils::input::keyboard::IsKeyDown(KEY_LEFT_CONTROL))
+            utils::DebugPrintContent();
+    }
+};
+
+#endif //SMARTIN3D_XWINGDESTROYJOB_H
