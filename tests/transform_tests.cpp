582601934857e0452e7a01011f49cc28b5f640f0
Pavel Loginov <lgnv2009@gmail.com>
2021-03-08 13:50:49 +0300

    Don't use heap in tests

--- tests/transform_tests.cpp
+++ tests/transform_tests.cpp
@@ -6,52 +6,46 @@
 #include "base/Transform.h"
 
 TEST_CASE("Transform default constructor", "[require]") {
-    smartin::base::Transform* transform = new smartin::base::Transform();
+    smartin::base::Transform transform;
 
-    REQUIRE_EQ(transform->GetPosition(), glm::vec3(0.0f, 0.0f, 0.0f));
-    REQUIRE_EQ(transform->GetSize(), glm::vec3(1.0f, 1.0f, 1.0f));
-    REQUIRE_EQ(transform->GetEulerAngles(), glm::vec3(0.0f, 0.0f, 0.0f));
+    REQUIRE_EQ(transform.GetPosition(), glm::vec3(0.0f, 0.0f, 0.0f));
+    REQUIRE_EQ(transform.GetSize(), glm::vec3(1.0f, 1.0f, 1.0f));
+    REQUIRE_EQ(transform.GetEulerAngles(), glm::vec3(0.0f, 0.0f, 0.0f));
 
-    REQUIRE_EQ(transform->GetRight(), glm::vec3(1.0f, 0.0f, 0.0f));
-    REQUIRE_EQ(transform->GetUp(), glm::vec3(0.0f, 1.0f, 0.0f));
-    REQUIRE_EQ(transform->GetForward(), glm::vec3(0.0f, 0.0f, 1.0f));
-
-    delete transform;
+    REQUIRE_EQ(transform.GetRight(), glm::vec3(1.0f, 0.0f, 0.0f));
+    REQUIRE_EQ(transform.GetUp(), glm::vec3(0.0f, 1.0f, 0.0f));
+    REQUIRE_EQ(transform.GetForward(), glm::vec3(0.0f, 0.0f, 1.0f));
 }
 
 TEST_CASE("Transform 90 degrees rotation", "[require]") {
-    smartin::base::Transform* transform = new smartin::base::Transform();
+    smartin::base::Transform transform;
 
-    transform->RotateAround(glm::vec3(0.0f, 1.0f, 0.0f), 90.0f);
+    transform.RotateAround(glm::vec3(0.0f, 1.0f, 0.0f), 90.0f);
 
-    REQUIRE_EQ(transform->GetPosition(), glm::vec3(0.0f, 0.0f, 0.0f));
-    REQUIRE_EQ(transform->GetSize(), glm::vec3(1.0f, 1.0f, 1.0f));
-    REQUIRE_EQ_VEC3(transform->GetEulerAngles(), glm::vec3(0.0f, 90.0f, 0.0f), 1.0f);
+    REQUIRE_EQ(transform.GetPosition(), glm::vec3(0.0f, 0.0f, 0.0f));
+    REQUIRE_EQ(transform.GetSize(), glm::vec3(1.0f, 1.0f, 1.0f));
+    REQUIRE_EQ_VEC3(transform.GetEulerAngles(), glm::vec3(0.0f, 90.0f, 0.0f), 1.0f);
 
-    REQUIRE_EQ_VEC3(transform->GetRight(), glm::vec3(0.0f, 0.0f, -1.0f), 0.01f);
-    REQUIRE_EQ_VEC3(transform->GetUp(), glm::vec3(0.0f, 1.0f, 0.0f), 0.01f);
-    REQUIRE_EQ_VEC3(transform->GetForward(), glm::vec3(1.0f, 0.0f, 0.0f), 0.01f);
-
-    delete transform;
+    REQUIRE_EQ_VEC3(transform.GetRight(), glm::vec3(0.0f, 0.0f, -1.0f), 0.01f);
+    REQUIRE_EQ_VEC3(transform.GetUp(), glm::vec3(0.0f, 1.0f, 0.0f), 0.01f);
+    REQUIRE_EQ_VEC3(transform.GetForward(), glm::vec3(1.0f, 0.0f, 0.0f), 0.01f);
 }
 
 TEST_CASE("Transform rotation by euler angles", "[require]") {
-    smartin::base::Transform* transform = new smartin::base::Transform();
-    REQUIRE_EQ_VEC4(transform->GetRotation(), glm::quat(1.0f, 0.0f, 0.0f, 0.0f), 0.01f);
+    smartin::base::Transform transform;
+    REQUIRE_EQ_VEC4(transform.GetRotation(), glm::quat(1.0f, 0.0f, 0.0f, 0.0f), 0.01f);
 
-    transform->Rotate(glm::vec3(90.0f, 0.0f, 0.0f));
+    transform.Rotate(glm::vec3(90.0f, 0.0f, 0.0f));
 
-    REQUIRE_EQ(transform->GetPosition(), glm::vec3(0.0f, 0.0f, 0.0f));
-    REQUIRE_EQ(transform->GetSize(), glm::vec3(1.0f, 1.0f, 1.0f));
+    REQUIRE_EQ(transform.GetPosition(), glm::vec3(0.0f, 0.0f, 0.0f));
+    REQUIRE_EQ(transform.GetSize(), glm::vec3(1.0f, 1.0f, 1.0f));
 
-    REQUIRE_EQ_VEC3(transform->GetEulerAngles(), glm::vec3(90.0f, 0.0f, 0.0f), 1.0f);
-    REQUIRE_EQ_VEC4(transform->GetRotation(), glm::quat(0.707f, 0.707f, 0.0f, 0.0f), 0.01f);
+    REQUIRE_EQ_VEC3(transform.GetEulerAngles(), glm::vec3(90.0f, 0.0f, 0.0f), 1.0f);
+    REQUIRE_EQ_VEC4(transform.GetRotation(), glm::quat(0.707f, 0.707f, 0.0f, 0.0f), 0.01f);
 
-    REQUIRE_EQ_VEC3(transform->GetRight(), glm::vec3(1.0f, 0.0f, 0.0f), 0.01f);
-    REQUIRE_EQ_VEC3(transform->GetUp(), glm::vec3(0.0f, 0.0f, 1.0f), 0.01f);
-    REQUIRE_EQ_VEC3(transform->GetForward(), glm::vec3(0.0f, -1.0f, 0.0f), 0.01f);
-
-    delete transform;
+    REQUIRE_EQ_VEC3(transform.GetRight(), glm::vec3(1.0f, 0.0f, 0.0f), 0.01f);
+    REQUIRE_EQ_VEC3(transform.GetUp(), glm::vec3(0.0f, 0.0f, 1.0f), 0.01f);
+    REQUIRE_EQ_VEC3(transform.GetForward(), glm::vec3(0.0f, -1.0f, 0.0f), 0.01f);
 }
 
 
