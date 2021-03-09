397c31f1167fd6b286c54559470970e1757e663f
Pavel Loginov <lgnv2009@gmail.com>
2021-03-09 19:47:16 +0300

    Cleaned up code of the sample app

--- samples/flying_camera/src/main.cpp
+++ samples/flying_camera/src/main.cpp
@@ -6,36 +6,32 @@
 using namespace smartin;
 
 int main() {
-    auto app = new base::App("Test", 1280, 720);
-    app->Init();
+    base::App app("Test", 1280, 720);
+    app.Init();
+    app.SetTargetFPS(120);
 
-    app->CreateCamera(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, -60.0f, 0.0f));
-    app->SetSkybox({
-       "cupertin-lake_rt.tga",
-       "cupertin-lake_lf.tga",
-       "cupertin-lake_up.tga",
-       "cupertin-lake_dn.tga",
-       "cupertin-lake_bk.tga",
-       "cupertin-lake_ft.tga"
+    app.CreateCamera(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, -60.0f, 0.0f));
+    app.SetSkybox({
+        "cupertin-lake_rt.tga",
+        "cupertin-lake_lf.tga",
+        "cupertin-lake_up.tga",
+        "cupertin-lake_dn.tga",
+        "cupertin-lake_bk.tga",
+        "cupertin-lake_ft.tga"
     });
 
-    app->AddActor("xwing", "x-wing.obj",
-                  glm::vec3(-7.0f, 0.0f, 10.0f),
-                  glm::vec3(0.006f, 0.006f, 0.006f));
-    app->AddActor("blackhawk", "uh60.obj",
-                  glm::vec3(-8.0f, 2.0f, 0.0f),
-                  glm::vec3(0.4f, 0.4f, 0.4f),
-                  glm::vec3(-90.0f, 0.0f, 0.0f));
-    app->AddActor("cone", "cone.obj",
-                  glm::vec3(-8.0f, 2.0f, 5.0f));
+    app.AddActor("xwing", "x-wing.obj",
+                 glm::vec3(-7.0f, 0.0f, 10.0f),
+                 glm::vec3(0.006f, 0.006f, 0.006f));
+    app.AddActor("blackhawk", "uh60.obj",
+                 glm::vec3(-8.0f, 2.0f, 0.0f),
+                 glm::vec3(0.4f, 0.4f, 0.4f),
+                 glm::vec3(-90.0f, 0.0f, 0.0f));
+    app.AddActor("cone", "cone.obj",
+                 glm::vec3(-8.0f, 2.0f, 5.0f));
 
-    app->AddJob(std::make_unique<CameraMovementJob>(app->GetCamera(), 10.0f, 1.0f));
-    app->AddJob(std::make_unique<XwingDestroyJob>());
+    app.AddJob(std::make_unique<CameraMovementJob>(app.GetCamera(), 10.0f, 1.0f));
+    app.AddJob(std::make_unique<XwingDestroyJob>());
 
-    app->SetTargetFPS(120);
-
-    app->Run();
-    delete app;
-
-    return 0;
+    app.Run();
 }
